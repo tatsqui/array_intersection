de3b17ced8aa6e0f69ba4d16ec6ecc1fecb648b8
shrutivanw <shruti.dalvi@gmail.com>
2019-02-05 09:35:16 -0800

    C11 updates - improved instructions
    Improved instructions for the assignment.

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,6 @@
-# Creates a new array to return the intersection of the two input arrays
+# Returns a new array to that contains elements in the intersection of the two input arrays
+# Time complexity: ?
+# Space complexity: ?
 def intersection(array1, array2)
   raise NotImplementedError
 end
