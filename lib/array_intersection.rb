f4954638c842826b790b9ea2d7b286a7584e2fce
Tatiana Quintana <tatsqui@gmail.com>
2019-04-16 08:54:13 -0700

    used sort to solve this problem.

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,6 +1,27 @@
 # Returns a new array to that contains elements in the intersection of the two input arrays
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: n*m
+# Space complexity: Can be On (sorting in place possible)
 def intersection(array1, array2)
-  raise NotImplementedError
+  unless array1 && array2
+    return []
+  end
+
+  if array1.length < array2.length
+    smaller = array1.sort
+   larger = array2.sort
+  else
+    smaller = array2.sort
+   larger = array1.sort
+  end
+
+  i = 0
+  result = []
+  smaller.each do |num|
+    larger.each do |other|
+      if num == other
+        result << num
+      end
+    end
+  end
+  return result
 end
